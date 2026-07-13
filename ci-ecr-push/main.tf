fffef28af4dbc220e338371ac835ab910ab8523d
richard.smith <richard.smith@dendra.io>
2026-07-13 15:13:56 +0100

    fix: reference to oidc provider data object

--- ci-ecr-push/main.tf
+++ ci-ecr-push/main.tf
@@ -7,7 +7,7 @@ data "aws_iam_policy_document" "github_oidc_assume_role" {
     actions = ["sts:AssumeRoleWithWebIdentity"]
     principals {
       type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.github.arn]
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
     }
 
     condition {
